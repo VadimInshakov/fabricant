8ead68ba2986b686c249f01c76e99b4c5288b96c
Vadim Inshakov <vadiminshakov@gmail.com>
2019-10-06 22:21:37 +0300

    fix signatures

--- broker/interfaces.go
+++ broker/interfaces.go
@@ -3,20 +3,21 @@ package broker
 import "github.com/vadiminshakov/exmo"
 
 type Fabricator interface {
-	Sell(sell, buy string, volume, price float64) string
-	MarketBuy(buy, sell string, volume float64) string
-	Buy(buy, sell string, volume, price float64) string
+	Sell(string, string, float64, float64) string
+	MarketBuy(string, string, float64) string
+	Buy(string, string, float64, float64) string
 	WaitOrdersExecute()
-	WhatICanBuy(buy, sell string) (float64, error)
-	WhatICanSell(currency string) float64
+	WhatICanBuy(string, string) (float64, error)
+	WhatICanSell(string) float64
 	Monitor()
-	WaitForBuy(buy, sell string, price float64) string
-	Save(key float64, value Order) error
-	Delete(key float64)
+	WaitForBuy(string, string, float64) string
+	Save(float64, Order) error
+	Delete(float64)
 	GetConfig() Config
 	GetTimers() Timers
 	GetApi() *exmo.Exmo
 	GetMeta() Meta
+	SetMetaSelled(float64)
 	GetOrders() (map[float64]Order, error)
-	GetLastTradePriceForPair(buy, sell string) float64
+	GetLastTradePriceForPair(string, string) float64
 }
